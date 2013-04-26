d2fdf4ce225921ca70a0d12b888ea284266c5355
Johnpel Quingua <johnpelquingua@gmail.com>
2013-04-26 10:25:52 +0800

    Updated Charts and Media
    meh~

--- site/views/Charts/tmpl/default.php
+++ site/views/Charts/tmpl/default.php
@@ -26,9 +26,56 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 
 /** uncomment out to display a specific custom field */
 //echo $custom_fields->image1;
-?>
-<div>
 
-    <h1>Charts</h1>
-	<p>New Version</p>
-</div>
+// Add a reference to a Javascript file
+// The default path is 'media/system/js/'
+
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Highstock Example</title>
+
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript">
+$(function() {
+//$.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
+	$.getJSON('<?php echo $this->baseurl ?>/media/<?php echo $this->media;?>com_stockinfos/js/jsonp.php', function(data) {
+		// Create the chart
+		console.log(data);
+		$('#container').highcharts('StockChart', {
+			
+
+			rangeSelector : {
+				selected : 1
+			},
+
+			title : {
+				text : 'Lopez Holding Corp'
+			},
+			
+			series : [{
+				name : 'LPZ',
+				data : data,
+				tooltip: {
+					valueDecimals: 2
+				}
+			}]
+		});
+	});
+
+});
+
+		</script>
+</head> 
+    
+	<body>
+    
+<script src="<?php echo $this->baseurl ?>/media/<?php echo $this->media;?>com_stockinfos/js/highstock.js" ></script>
+<script src="<?php echo $this->baseurl ?>/media/<?php echo $this->media;?>com_stockinfos/js/modules/exporting.js" ></script>
+
+<div id="container" style="height: 500px; min-width: 500px"></div>
+	</body>
+</html>
+
+
