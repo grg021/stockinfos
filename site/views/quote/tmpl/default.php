213336d08d2b3ac631a0629e3e7d8b149b9f647b
Russel John Villanueva <semrussel@gmail.com>
2013-04-29 18:33:24 -0700

    updated quote page
    more on computation

--- site/views/quote/tmpl/default.php
+++ site/views/quote/tmpl/default.php
@@ -26,15 +26,6 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 
 /** uncomment out to display a specific custom field */
 //echo $custom_fields->image1;
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
-$conf =& JFactory::getConfig();
-
-$host = $conf->getValue('config.host');
-$user = $conf->getValue('config.user');
-$password = $conf->getValue('config.password');
-$database = $conf->getValue('config.db');
-$prefix = $conf->getValue('config.dbprefix');
-$table = "$prefix"."stockinfos";
 ?>
 <style type="text/css">
 table.hovertable {
@@ -53,7 +44,7 @@ table.hovertable th {
 	border-bottom: 1px solid #fff;
 	color: #039;
 	padding: 8px;
-	padding-right:88px
+	padding-right:75px
 }
 table.hovertable tr {
 	background-color:#d0dafd;
@@ -67,66 +58,145 @@ table.hovertable td {
 }
 </style>
 <div>
-    
+    <h1>History Share Price</h1>
+	
 	<?php
-	mysql_connect("$host", "$user", "$password") or die(mysql_error()); 
-	mysql_select_db("$database") or die(mysql_error()); 
-	$data = mysql_query("SELECT * FROM $table order by AsOfDate desc limit 1;") 
-	or die(mysql_error());
-	while($info = mysql_fetch_array( $data )) 
-		{
+	error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+		$selectyear = $_POST['year'];
+		$selectmonth = $_POST['month'];
+		if($selectmonth == January){
+		$selmon = "01";
+		}
+		else if($selectmonth == February){
+		$selmon = "02";
+		}
+		else if($selectmonth == March){
+		$selmon = "03";
+		}
+		else if($selectmonth == April){
+		$selmon = "04";
+		}
+		else if($selectmonth == May){
+		$selmon = "05";
+		}
+		else if($selectmonth == June){
+		$selmon = "06";
+		}
+		else if($selectmonth == July){
+		$selmon = "07";
+		}
+		else if($selectmonth == August){
+		$selmon = "08";
+		}
+		else if($selectmonth == September){
+		$selmon = "09";
+		}
+		else if($selectmonth == October){
+		$selmon = "10";
+		}
+		else if($selectmonth == November){
+		$selmon = "11";
+		}
+		else if($selectmonth == December){
+		$selmon = "12";
+		}
+		$yearnow = date("Y");
+		$monnow = date("m");
+		$datenow = date("d");
+		$call1 = "$selectyear"."-"."$selmon"."-"."01";
+		$call2 = "$selectyear"."-"."$selmon"."-"."31";
 	?>
 	
-	<h1>Quote for <?php echo $info['AsOfDate']; ?> </h1>
-	
-	
+	<form action="<?php echo JRoute::_('index.php')?>" method="POST">
+	<select name="year" class="body">
+                          <?php
+						  for($i=$yearnow;$i>=2000;$i--){
+						  ?>                  
+                                            <option value="<?php echo"$i"; ?>"><?php echo"$i"; ?></option>
+                                           
+									<?php } ?>
+								</select>
+	               
+                                    <?php
+								$curr_month = date("m");
+								$month = array (01=>"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+								$select = "<select name=\"month\">\n";
+								foreach ($month as $key => $val) {
+									$select .= "\t<option val=\"".$key."\"";
+									if ($key == $curr_month) {
+									$wew = $key;
+									$selected =	$select .= " selected=\"selected\">".$val."</option>\n"; ;
+									} else {
+										$select .= ">".$val."</option>\n";
+									}
+								}
+								$select .= "</select>";
+								echo $select;
+								?>
+								
+								
+	<input type="submit" value="Enter" name="submit">
+	</form>
+	<br>
 	<table class="hovertable">
 	
 	<tr>
-		<th>PSE : LPZ</th>
-		<th>Listed Investments</th>
+		<th>Date</th>
+		<th>Open</th>
+		<th>High</th>
+		<th>Low</th>
+		<th>Close</th>
+		<th>Volume</th>
 	</tr>
+	
+	<?php
+	error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+$conf =& JFactory::getConfig();
+
+$host = $conf->getValue('config.host');
+$user = $conf->getValue('config.user');
+$password = $conf->getValue('config.password');
+$database = $conf->getValue('config.db');
+$prefix = $conf->getValue('config.dbprefix');
+$table = "$prefix"."stockinfos";
+	mysql_connect("$host", "$user", "$password") or die(mysql_error()); 
+	mysql_select_db("$database") or die(mysql_error()); 
+	
+	//if the user already entered desired year
+	if($_POST['year'] != NULL){
+	$data = mysql_query("SELECT * FROM $table WHERE `AsOfDate`<='$call2' AND `AsOfDate`>='$call1' order by AsOfDate desc limit 10") 
+	or die(mysql_error());
+	while($info = mysql_fetch_array( $data ))
+		{
+		
+	?>
 	
 	<tr onmouseover="this.style.backgroundColor='#d0dafd';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>Open : <?php echo $info['open_value']; ?></td>
-		<td>ABS - CBN : <?php echo $info['abscbn']; ?> </td>
+		<td><?php echo $info['AsOfDate'];?></td>
+		<td><?php echo $info['open_value'];?></td>
+		<td><?php echo $info['high'];?></td>
+		<td><?php echo $info['low'];?></td>
+		<td><?php echo $info['close_value'];?></td>
+		<td><?php echo $info['volume'];?></td>
 	</tr>
+	<?php }
+	}
+	else{
+	$data = mysql_query("SELECT * FROM $table order by AsOfDate desc limit 10") 
+	or die(mysql_error());
+	while($info = mysql_fetch_array( $data ))
+		{
+		
+	?>
 	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>High : <?php echo $info['high']; ?></td>
-		<td>ABS - CBN PDR : <?php echo $info['abscbnpdr']; ?></td>
+	<tr onmouseover="this.style.backgroundColor='#d0dafd';" onmouseout="this.style.backgroundColor='#e8edff';">
+		<td><?php echo $info['AsOfDate'];?></td>
+		<td><?php echo $info['open_value'];?></td>
+		<td><?php echo $info['high'];?></td>
+		<td><?php echo $info['low'];?></td>
+		<td><?php echo $info['close_value'];?></td>
+		<td><?php echo $info['volume'];?></td>
 	</tr>
-	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>Close : <?php echo $info['close_value']; ?></td>
-		<td>FPHC : <?php echo $info['fphc']; ?></td>
-	</tr>
-	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>Change : *computation</td>
-		<td>FPHC Preferred : <?php echo $info['fphc preferred']; ?></td>
-	</tr>
-	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>Volume : <?php echo $info['volume']; ?></td>
-		<td>FGEN : <?php echo $info['fgen']; ?></td>
-	</tr>
-	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>52 Week High : *computation</td>
-		<td>EDC : <?php echo $info['edc']; ?></td>
-	</tr>
-	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td>52 Week Low : *computation</td>
-		<td>MERALCO  : <?php echo $info['meralco']; ?></td>
-	</tr>
-	
-	<tr onmouseover="this.style.backgroundColor='#ffff66';" onmouseout="this.style.backgroundColor='#e8edff';">
-		<td></td>
-		<td>ROCK : <?php echo $info['rock']; ?></td>
-	</tr>
-	<?php } ?>
-	
-	</table>
+	<?php } } ?>
+	</table> 
 </div>
